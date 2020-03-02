5070fec3f3bbc19b0eb108f1d9dd6b4cf790a6e4
Tatyana Shurinova <tatyana.shurinova@gmail.com>
2020-03-02 23:12:43 +0200

    fixed  slider(infinite: false). Add cursor: pointer

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ $(function() {});
 
 $(" .slider__inner").slick({
   nextArrow: '<button type="button" class=" slick-btn slick-next"></button>',
-  prevArrow: '<button type="button" class="slick-btn slick-prev"></button>'
+  prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+  infinite: false
 });
